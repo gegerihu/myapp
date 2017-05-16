9904cb1a928a662dc7ae8142b1066d84921d30df
gegerihu <gegerihu@gmail.com>
2017-05-16 17:51:23 +0800

    新闻list模板完成
    news list

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,43 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('layouts/bootswatch', { title: '传媒系' });
+  res.render('layouts/bootswatch', { title: '传媒系' ,
+        documentList:[{
+    title:'关于大大大所大大打算打算大的1',
+    img:'http://www.html-js.cn/upload/smallimgs/img20160515113840.jpg',
+    date: 222,
+    },
+    {
+    title:'关于大大大所大大打算打算大的2',
+    img:'/images/1.jpg',
+    date: 333,
+    },
+    {
+    title:'关于大大大所大大打算打算大的3',
+    img:'/images/1.jpg',
+    date: 444,
+    }]
+});
 });
 
+router.get('/news', function(req, res, next) {
+  res.render('layouts/news', { title: '传媒系' ,
+    documentList:[{
+    title:'关于大大大所大大打算打算大的1',
+    img:'http://www.html-js.cn/upload/smallimgs/img20160515113840.jpg',
+    date: 222,
+    },
+    {
+    title:'关于大大大所大大打算打算大的2',
+    img:'/images/1.jpg',
+    date: 333,
+    },
+    {
+    title:'关于大大大所大大打算打算大的3',
+    img:'/images/1.jpg',
+    date: 444,
+    }]
+   });
+});
 module.exports = router;
 
